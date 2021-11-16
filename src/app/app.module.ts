f095b77355cfd0cf67b9611449c0bd8e10d1b64c
Christopher Pearson <cjpearson@hotmail.com>
2021-11-16 22:20:42 +0000

    update app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormatDisplayPipe } from './format-display.pipe';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormatDisplayPipe
+    FormatDisplayPipe,
+    HeaderComponent,
+    FooterComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule
